f43343eac41f560c41ba8b7f863763ba9f6616d2
michaelindenkampen <michael.indenkaempen@gmail.com>
2023-05-19 05:15:22 +0200

    Spanish Regions Data

--- graphs_manual.py
+++ graphs_manual.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pytrends.request import TrendReq
 from matplotlib import pyplot as plt
 
+
 #Change the variable 'main_directory' to your local export path:
 main_directory  = '/Users/michaelindenkampen/PycharmProjects/GoogleTrends/'
 
@@ -15,42 +16,30 @@ region_code = ['AN','AR','AS','IB','PV','CN','CB','CL','CM','CT','EX','GA','RI',
 region_name = ['Andalusia','Aragon','Asturias','Balearic Islands','Basque Country','Canary Islands','Cantabria','Castile and Leon','Castile-La Mancha','Catalonia','Extremadura','Galicia','La Rioja','Community of Madrid','Region of Murcia','Navarre','Valencian Community','Ceuta','Melila']
 region_info = pd.DataFrame({'region_code': region_code, 'region_name': region_name})
 
-pytrend = TrendReq()
-
-
-#Download the timeseries data for all spanish regions (2005 - 2014) and save them to the file directory saved in 'main_directory'
-for region_i in region_code:
-    pytrend.build_payload(kw_list= [search_term], timeframe='2005-01-01 2014-12-31', geo= 'ES-' + region_i)
-
-    df = pytrend.interest_over_time()
-
-    df.to_csv(main_directory+ 'csv_export/' + region_i + '.csv')
-
-
-#Download the timeseries for spain and save it to the file directory saved in 'main_directory'.
-pytrend.build_payload(kw_list= [search_term] , timeframe='2005-01-01 2014-12-31', geo= 'ES')
-
-df = pytrend.interest_over_time()
-
-df.to_csv(main_directory + 'csv_export/' + 'Spain' + '.csv')
-
-del df
-
 
 #Make regional graphs and export them to /graphs.
 for region_i in region_code:
-    df = pd.read_csv(main_directory + 'csv_export/' + region_i +'.csv', parse_dates= ['date'], index_col = ['date'])
+    df = pd.read_csv(main_directory + 'csv_manual/' + region_i +'_m.csv', skiprows = 1)
+    df.rename(columns={'Monat': 'date', df.columns[1]: 'Calima'}, inplace=True)
+    df['date'] = pd.to_datetime(df['date'])
+    df.set_index('date', inplace=True)
+    df['Calima'] = df['Calima'].replace('<1', '0')
+    df['Calima'] = df['Calima'].astype(int)
     df.plot(y = search_term, legend = None,  color='black', figsize=(10, 6))
     plt.title(region_info.loc[region_info['region_code'] == region_i, 'region_name'].values[0])
     plt.xlabel('')
     plt.ylabel('Search Interest')
-    plt.savefig(main_directory + 'graphs/' + region_info.loc[region_info['region_code'] == region_i, 'region_name'].values[0] + '.pdf', format="pdf")
+    plt.savefig(main_directory + 'graphs_manual/' + region_info.loc[region_info['region_code'] == region_i, 'region_name'].values[0] + '_m.pdf', format="pdf")
+    plt.close('all')
 
 #Make graph for spain and export it to /graphs
-df = pd.read_csv(main_directory + 'csv_export/Spain.csv', parse_dates=['date'], index_col=['date'])
+df = pd.read_csv(main_directory + 'csv_manual/Spain_m.csv', skiprows= 1)
+df.rename(columns={'Monat': 'date', 'Calima: (Spanien)': 'Calima'}, inplace=True)
+df['date'] = pd.to_datetime(df['date'])
+df.set_index('date', inplace=True)
 plt.style.use('ggplot')
 df.plot(y= search_term, legend=None,  color='black', figsize=(10, 6))
 plt.title('Spain')
 plt.xlabel('')
 plt.ylabel('Search Interest')
-plt.savefig(main_directory + 'graphs/Spain.pdf', format="pdf")
+plt.savefig(main_directory + 'graphs_manual/Spain_m.pdf', format="pdf")
