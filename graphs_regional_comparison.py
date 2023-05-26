04c26efa1f373097e386903a96053cdef8833f74
michaelindenkampen <michael.indenkaempen@gmail.com>
2023-05-26 11:11:36 +0200

    Spanish Regions Data

--- graphs_regional_comparison.py
+++ graphs_regional_comparison.py
@@ -6,7 +6,11 @@
 
 #This section imports the packages that are requires to run this script.
 import pandas as pd
+import plotly_express as px
 from matplotlib import pyplot as plt
+import geopandas as gpd
+import pandas as pd
+import seaborn as sns
 
 #Setup: To create the graphs, please enter your local directory below.
 #Change the variable 'main_directory' to your local export path:
@@ -32,7 +36,7 @@ plt.style.use('ggplot')
 plt.figure(figsize=(25, 10))
 plt.xticks(rotation=45)
 plt.bar(df['region'],df['calima'], color = 'navy' )
-plt.title('Search Interest - Regional Comparison')
+plt.title('Search Interest - Autonomous Communities')
 plt.ylabel('Search Interest')
 plt.subplots_adjust(left=0.05, right=0.95, bottom=0.25, top=0.9)
 plt.savefig(main_directory + 'graphs_manual_regional_comparison/Spain_regional_comparison.pdf', format="pdf")
@@ -65,3 +69,52 @@ plt.savefig(main_directory + 'graphs_manual_regional_comparison/Spain_cities_com
 plt.close('all')
 del df
 
+
+
+#Make regional comparison graphs:
+#Boundary shp. files found in ESP_adm are available at: http://www.diva-gis.org/gdata
+
+boundary_shp = gpd.read_file(main_directory + '/csv_manual_regional_comparison/ESP_adm/ESP_adm1.shp')
+
+
+#Read in regional data / I drop regions with no active search interest.
+df = pd.read_csv(main_directory + 'csv_manual_regional_comparison/Spain_comparison_regions_en.csv', skiprows= 1)
+df.rename(columns={df.columns[0]: 'region', df.columns[1]: 'calima'}, inplace=True)
+
+#To rescale the data, such that the largest item after the canary islands is scaled to index value  = 100, I drop the value for the canary islands,
+#aswell as the two regions without any observation. I then rescale the entire column.
+df = df.drop(df.index[0])
+df = df.drop(df.index[16])
+df = df.drop(df.index[16])
+df['calima'] = df['calima'].astype(int)
+df['calima'] = (df['calima'] / df['calima'].iloc[0]) * 100
+
+ID_1 = [11,3,10,13,1,8,17,6,16,12,2,4,18,5,9,15]
+
+df['ID_1'] = ID_1
+
+
+# Merge dataframes
+map = boundary_shp.set_index('ID_1').join(df.set_index('ID_1'))
+map = map.dropna(subset=['calima'])
+
+# Plot
+fig, ax = plt.subplots(1, 1)
+map.plot(column='calima', ax=ax, legend=False, cmap='Blues')
+
+for idx, row in map.iterrows():
+    centroid = row['geometry'].centroid
+    value = round(row['calima'])
+    ax.text(centroid.x, centroid.y, str(value), ha='center', va='center', weight='bold')
+
+# Remove the axis
+ax.axis('off')
+plt.title('Search Interest - Autonomous Communities')
+plt.subplots_adjust(left=0.005, right=0.995, bottom=0.005, top=0.9)
+plt.show()
+
+plt.savefig(main_directory + 'graphs_manual_regional_comparison/Spain_regions_comparison_map.pdf', format="pdf")
+
+
+del map
+del df
